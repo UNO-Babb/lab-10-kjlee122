71fe622b660a3235c6b051c129adb2a945dfffad
Kjlee122 <147563359+kjlee122@users.noreply.github.com>
2025-11-18 05:39:56 +0000

    fin

--- main.py
+++ main.py
@@ -2,3 +2,23 @@
 #Name:
 #Date:
 #Assignment:
+
+import video_games
+import pandas
+import matplotlib.pyplot as plt
+video_game = video_games.get_video_game()
+reviews = []
+years = []
+
+
+for game in video_game:
+    review = game["Metrics"]["Review Score"]
+    year = game["Release"]["Year"]
+    reviews.append(review)
+    years.append(year)
+
+df = pandas.DataFrame([{"Reviews" : review, 
+                        "Years": year}])
+print(df)
+plt.plot(years, reviews, 'ro')
+plt.savefig("output")
